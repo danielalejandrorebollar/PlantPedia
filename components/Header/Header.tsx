f4cdadb45a5625124d8fe64408ddc7a40dd66caa
Jonathan Alvarez <j@jonalvarezz.com>
2021-06-02 21:21:32 +0200

    TopStories: Add index and author page
    commit_editmsg
    
    q

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,11 @@
+import { PropsWithChildren } from 'react'
+import Link, { LinkProps } from 'next/link'
 import { NavBar } from '@ui/NavBar'
+<<<<<<< HEAD
 import NavLink from 'next/link'
+=======
+import { Button } from '@ui/Button'
+>>>>>>> f4f38b7 (TopStories: Add index and author page)
 
 export function Header() {
   const items = ["language"]
@@ -7,10 +13,25 @@ export function Header() {
     <div className="mx-auto" style={{ maxWidth: '98%' }}>
       <NavBar title="🌿 Plantpedia">
         <div>
+<<<<<<< HEAD
         <NavLink href="/top-stories">Top Stories</NavLink>
         </div>
         <div>{items}</div>
+=======
+          <NavLink href="/top-stories">Top Stories</NavLink>
+        </div>
+>>>>>>> f4f38b7 (TopStories: Add index and author page)
       </NavBar>
     </div>
+  )
+}
+
+function NavLink({ children, ...linkProps }: PropsWithChildren<LinkProps>) {
+  return (
+    <Link {...linkProps} passHref>
+      <Button color="inherit" variant="text" component="a">
+        {children}
+      </Button>
+    </Link>
   )
 }
