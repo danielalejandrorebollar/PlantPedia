89e78681f47436210a99206401b1780cb07a6dcd
Daniel <dalandro@gmail.com>
2026-06-08 19:36:56 -0600

    agregando topstories

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { NavBar } from '@ui/NavBar'
+import NavLink from 'next/link'
 
 export function Header() {
   const items = ["language"]
   return (
     <div className="mx-auto" style={{ maxWidth: '98%' }}>
       <NavBar title="🌿 Plantpedia">
+        <div>
+        <NavLink href="/top-stories">Top Stories</NavLink>
+        </div>
         <div>{items}</div>
       </NavBar>
     </div>
