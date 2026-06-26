25ccbb7a12d95ba1b4f87efa7a75687e6e95041e
Daniel <dalandro@gmail.com>
2026-06-26 11:15:50 -0600

    fixing i18nConf v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async ({locale}) =>{
   
   console.log('locale =', locale)
   try {
-    // const plants = await getPlantList({ limit: 20, locale})
+    const plants = await getPlantList({ limit: 20, locale})
     // console.log("Plantas en getStaticProps de Index general",plants)
     const i18nConf = await serverSideTranslations(locale! , ['common'], nextI18NextConfig)
     console.log(i18nConf)
